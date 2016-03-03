5a9aaadc09ab26b49818015f1e8487dea50eeef6
Lucklyric <xinyao1@ualberta.ca>
2016-03-03 12:31:06 -0700

    1. Change some structures

--- mm811as3-homwork/twosensorsfusion.py
+++ mm811as3-homwork/twosensorsfusion.py
@@ -24,25 +24,31 @@ testOutputs = testData[0:,11:12].astype(np.int32)
 
 theautil.joint_shuffle(inputs,outputs)
 
-#train_and_valid, test = theautil.split_validation(90, inputs, outputs)
-train, valid = theautil.split_validation(90,inputs,outputs)
+train_and_valid, test = theautil.split_validation(90, inputs, outputs)
+train, valid = theautil.split_validation(90,train_and_valid[0],train_and_valid[1])
 
 def linit(x):
     return x.reshape((len(x),))
 
 train = (train[0],linit(train[1]))
 valid = (valid[0],linit(valid[1]))
-test  = (testInputs ,linit(testOutputs))
+test = (test[0],linit(test[1]))
+testNew  = (testInputs ,linit(testOutputs))
 
-net = theanets.Classifier([11,50,100,200,400,200,100,100,50,25,2])
-net.train(train, valid, algo='layerwise', max_updates=mupdates, patience=1)
+net = theanets.Classifier([11,(100,'softmax'),(500,'softmax'),(1000,'softmax'),(500,'softmax'),(250,'softmax'),(100,'softmax'),(2,'softmax')])
+#net = theanets.Classifier([11,(500,'sigmoid'),(1000,'sigmoid'),(500,'sigmoid'),(2,'sigmoid')])
+net.train(train, valid, algo='layerwise', max_updates=mupdates, patience=1,save_every=100,save_progress="model{}")
 print "Finsh and Save model"
-net.save()
 print "Learner on the test set"
 classify = net.classify(test[0])
 print "%s / %s " % (sum(classify == test[1]),len(test[1]))
 print collections.Counter(classify)
 print theautil.classifications(classify,test[1])
+print "Learner on the New test set"
+classify = net.classify(testNew[0])
+print "%s / %s " % (sum(classify == testNew[1]),len(testNew[1]))
+print collections.Counter(classify)
+print theautil.classifications(classify,testNew[1])
 
 
 
